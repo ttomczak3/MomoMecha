e94a206b44254c074aa05f65f9a55eba7d2cbc07
Tristen Tomczak <ttomczak1512@protonmail.com>
2024-02-01 22:42:12 -0600

    add: HTMX

--- MomoMecha/Pages/Error.cshtml.cs
+++ MomoMecha/Pages/Error.cshtml.cs
@@ -8,7 +8,7 @@ namespace MomoMecha.Pages
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
