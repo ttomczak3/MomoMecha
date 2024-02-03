12e298d70f99729b1ce09b42da26de1424140fcd
Tristen Tomczak <ttomczak1512@protonmail.com>
2024-02-03 17:50:06 -0600

    update: user validation for backlog and UX

--- MomoMecha/Pages/BacklogPages/Create.cshtml.cs
+++ MomoMecha/Pages/BacklogPages/Create.cshtml.cs
@@ -31,7 +31,40 @@ namespace MomoMecha.Pages.BacklogPages
         [BindProperty]
         public Backlog Backlog { get; set; } = default!;
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public string[] SeriesItems { get; } = [
+            "Universal Century",
+            "Witch From Mercury",
+            "IBO",
+            "Build Fighters/Divers",
+            "Gundam Wing",
+            "Gundam Seed",
+            "Gundam 00",
+            "Gundam Thunderbolt",
+            "G Gundam",
+            "Gundam Age",
+            "Gundam F91"
+        ];
+
+        public string[] GradeItems { get; } = [
+            "MG",
+            "RG",
+            "PG",
+            "HG",
+            "SD",
+            "RE",
+            "RE/100",
+            "FM",
+            "EG"
+        ];
+
+        public string[] ScaleItems { get; } = [
+            "1/144",
+            "1/100",
+            "1/60",
+            "1/72",
+            "1/48"
+        ];
+
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
--- MomoMecha/Pages/BacklogPages/Edit.cshtml.cs
+++ MomoMecha/Pages/BacklogPages/Edit.cshtml.cs
@@ -23,6 +23,40 @@ namespace MomoMecha.Pages.BacklogPages
         [BindProperty]
         public Backlog Backlog { get; set; } = default!;
 
+        public string[] SeriesItems { get; } = [
+            "Universal Century",
+            "Witch From Mercury",
+            "IBO",
+            "Build Fighters/Divers",
+            "Gundam Wing",
+            "Gundam Seed",
+            "Gundam 00",
+            "Gundam Thunderbolt",
+            "G Gundam",
+            "Gundam Age",
+            "Gundam F91"
+        ];
+
+        public string[] GradeItems { get; } = [
+            "MG",
+            "RG",
+            "PG",
+            "HG",
+            "SD",
+            "RE",
+            "RE/100",
+            "FM",
+            "EG"
+        ];
+
+        public string[] ScaleItems { get; } = [
+            "1/144",
+            "1/100",
+            "1/60",
+            "1/72",
+            "1/48"
+        ];
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
