8024dcc10e50f48dfd16b248d20560c45068192b
Tristen Tomczak <ttomczak1512@protonmail.com>
2024-02-08 00:55:35 -0600

    feat: google external login

--- MomoMecha/Program.cs
+++ MomoMecha/Program.cs
@@ -6,6 +6,8 @@ using MomoMecha.Models;
 using MomoMecha.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -16,6 +18,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
+
+services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = "";
+    googleOptions.ClientSecret = "";
+});
 
 builder.Services.AddScoped<PhotoService>();
 builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
