41aea852d169202ff6211a53044892c204674ccb
Tristen Tomczak <ttomczak1512@protonmail.com>
2024-02-17 00:58:41 -0600

    update

--- MomoMecha/Models/ApplicationUser.cs
+++ MomoMecha/Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace MomoMecha.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public List<Gundam> Gundams { get; set; }
         public List<Backlog> Backlogs { get; set; }
+        public List<Wishlist> Wishlist { get; set; }
     }
 }
