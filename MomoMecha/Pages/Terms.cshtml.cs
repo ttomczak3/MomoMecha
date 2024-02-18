66d1c7097660966bcb6af2c42f6412c9fb0adf55
Tristen Tomczak <ttomczak1512@protonmail.com>
2024-02-18 00:57:24 -0600

    add: terms of service

---
+++ MomoMecha/Pages/Terms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MomoMecha.Pages
+{
+    public class TermsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
